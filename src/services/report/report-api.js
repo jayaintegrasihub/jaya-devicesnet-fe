edc3d311d70ffb697a782451e6bfba2ce816b5ed
Xyzting <reyhan.fathir1110@gmail.com>
2025-03-11 23:06:38 +0700

    feat: timezone on summary data report

--- src/services/report/report-api.js
+++ src/services/report/report-api.js
@@ -3,11 +3,12 @@ import apiClient from '../api'
 export default {
   getReportCompletenessSummary(tenantId, type, startTime, endTime) {
     let queryString = ''
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
     if (type == 'All') {
-      queryString = `tenantId=${tenantId}&startTime=${startTime}&endTime=${endTime}`
+      queryString = `tenantId=${tenantId}&startTime=${startTime}&endTime=${endTime}&timezone=${userTimeZone}`
     } else {
-      queryString = `tenantId=${tenantId}&type=${type}&startTime=${startTime}&endTime=${endTime}`
+      queryString = `tenantId=${tenantId}&type=${type}&startTime=${startTime}&endTime=${endTime}&timezone=${userTimeZone}`
     }
 
     return apiClient.get(`telemetry/report-completeness/?${queryString}`)
